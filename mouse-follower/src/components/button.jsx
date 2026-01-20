dcfa20ce2068ec421dc629180ae9e96bb09c21fe
JB <177959714+jrxjb@users.noreply.github.com>
2026-01-20 16:58:19 -0400

    formas

--- mouse-follower/src/components/button.jsx
+++ mouse-follower/src/components/button.jsx
@@ -3,10 +3,18 @@ import './button.css'
 function ButtonRadom({enable,muestraV,style}){
     
     return(
-
-        <button onClick={muestraV}  style={style}  >
-        {enable} 
-        </button>
+        <>
+         <div className='potion-closed' onClick={muestraV}  style={style}>
+             {enable} 
+            <div className='potion-top'>
+                <div className='potion-middle'>
+                </div>
+            </div>
+            <div className='potion-body'>    
+            </div>
+        </div>
+      
+        </>
     )
 };
 
