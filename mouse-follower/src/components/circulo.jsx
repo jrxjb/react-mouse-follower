982a744871da1fad3214c12ef3b1811b9ff8aa0d
JB <177959714+jrxjb@users.noreply.github.com>
2026-01-24 23:15:16 -0400

    update

--- mouse-follower/src/components/circulo.jsx
+++ mouse-follower/src/components/circulo.jsx
@@ -4,11 +4,26 @@ function CirculoSeguidor({Texto,style,clickToLossProps}){
     return(
     
         <div className='npc' style={style} onClick={clickToLossProps} >
-            <div className='head'></div>
-            <div className='body'></div>
+            <div className='head'>
+            </div>
+            <div className='torso'>
+                <div className='body'>
+                </div>
+                <div className='arm-righ'>  
+                    <div className='arm-righ-body'></div>
+                </div>
+                <div className='arm-left'>
+                    <div className='arm-left-body'></div>
+                </div>
+            </div> 
+
             <div className='legs'>
-                <div className='leg-left'></div>
-                <div className='leg-right'></div>
+                <div className='leg-left'>
+                    <div className='short-left'></div>
+                </div>
+                <div className='leg-right'>
+                    <div className='short-right'></div>
+                </div>
             </div>
         </div>
     )
