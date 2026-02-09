21bbbfc226e02dd4211455cf7dbe46440ac882e8
JB <177959714+jrxjb@users.noreply.github.com>
2026-02-09 19:28:18 -0400

    update text and regeneration of npc

--- mouse-follower/src/components/textoPantalla.jsx
+++ mouse-follower/src/components/textoPantalla.jsx
@@ -9,7 +9,7 @@ const TextP =({style,text})=>{
         <p>Rules</p>
         <ul>
         <li>Do you have 15s to win </li>
-        <li>You need to click de bottle to win</li>
+        <li>You need to click the bottle to win</li>
         <li>You can't click the mounster</li>
         <li>To reset the game, click the bottle</li>
         </ul>
